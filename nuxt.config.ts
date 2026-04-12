7268a2098872f167a7cbb4e9555a788de62239bd
Taskeren <r0yalist@outlook.com>
2026-04-12 13:34:49 +0800

    Update compat-date of wrangler and switch to Bun

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2024-11-01",
+	compatibilityDate: "2026-04-07",
 	devtools: { enabled: true },
 
 	nitro: {
-		preset: "cloudflare_module",
+		preset: "cloudflare-module",
 
 		cloudflare: {
 			deployConfig: true,
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
 		}
 	},
 
-	modules: ["nitro-cloudflare-dev", "@nuxt/ui", "@nuxtjs/i18n"],
+	modules: ["@nuxt/ui", "@nuxtjs/i18n"],
 
 	i18n: {
 		vueI18n: "./i18n.config.ts",
